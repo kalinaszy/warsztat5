a7950c151404c7cea87c706b39261779d981efce
Kalina <kalina.szymczyk@gmail.com>
2019-05-10 16:52:01 +0200

    obsluga i wysiwetlanie plikow

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    file = models.ImageField(upload_to='photos')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
--- pinterest/urls.py
+++ pinterest/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+
+from django.conf.urls.static import static
+
+from core.views import IndexView, AddPhotoView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', IndexView.as_view()),
+    path('add_photo', AddPhotoView.as_view()),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
