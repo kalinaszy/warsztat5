dc1923acda27454f19d4e882cfc09903afeef742
Kalina <kalina.szymczyk@gmail.com>
2019-06-12 00:58:10 +0200

    changed models

--- core/models.py
+++ core/models.py
@@ -2,9 +2,27 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+LIKE_CHOICES = (
+    ('thumb', 'kciuk'),
+    ('heart', 'serce'),
+    ('kiss', 'buziak'),
+)
+
 class Photo(models.Model):
     file = models.ImageField(upload_to='photos')
     creation_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    type = models.CharField(choices=LIKE_CHOICES, max_length=10)
+
+    class Meta:
+        unique_together = (
+            ('photo', 'user')
+        )
+
+
 
 
